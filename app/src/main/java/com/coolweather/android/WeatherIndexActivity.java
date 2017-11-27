d00ff75cc718b9a21903cc5e6888c7fe0708dc93
Li Zhaobin <lzbin04@qq.com>
2017-11-27 16:49:17 +0800

    加入创建数据库和表的各项配置

--- app/src/main/java/com/coolweather/android/WeatherIndexActivity.java
+++ app/src/main/java/com/coolweather/android/WeatherIndexActivity.java
@@ -3,11 +3,11 @@ package com.coolweather.android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class WeatherIndexActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_weather_index);
     }
 }
